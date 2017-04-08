cca950915fc2a4ad17544e041da30bdf11a8b1be
scheinonme <scheindana@gmail.com>
2017-04-08 07:31:55 -0400

    tidy up

--- Brexit/js/animations.js
+++ Brexit/js/animations.js
@@ -1,21 +1,21 @@
-// var offset = $('.box2').offset();
-// var leftOffset = offset.left;
+var offset = $('.box2').offset();
+var leftOffset = offset.left;
 
-// $('.box2').animate({
-//   left: -leftOffset
-// }, 10000);
+$('.box2').animate({
+  left: -leftOffset
+}, 10000);
 
-// $(".draggable").draggable({
-//   containment: ".container",
-//   start: function() {
-//     $('.box2').stop();
-//   },
-//   stop: function() {
-//     offset = $('.box2').offset();
-//     leftOffset = offset.left;
+$(".draggable").draggable({
+  containment: ".container",
+  start: function() {
+    $('.box2').stop();
+  },
+  stop: function() {
+    offset = $('.box2').offset();
+    leftOffset = offset.left;
 
-//     $('.box2').animate({
-//       left: -leftOffset
-//     }, 10000);
-//   }
-// });
+    $('.box2').animate({
+      left: -leftOffset
+    }, 10000);
+  }
+});
